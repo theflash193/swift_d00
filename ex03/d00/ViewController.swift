e2a169c6dd6c6636d35a98f054a9e2d2c48e573d
Gordon RASS-KWASI <grass-kw@e2r5p5.42.fr>
2016-10-19 17:44:29 +0200

    la pression sur les touche affiche un message d'erreur

--- ex03/d00/ViewController.swift
+++ ex03/d00/ViewController.swift
@@ -40,36 +40,74 @@ class ViewController: UIViewController {
 
     @IBAction func action_touch_0(sender: AnyObject) {
         calcul.text = "0"
+        print("pression touch 0")
     }
 
     @IBAction func action_touch_1(sender: AnyObject) {
         calcul.text = "1"
+        print("pression touch 1")
     }
     
     @IBAction func action_touch_2(sender: AnyObject) {
         calcul.text = "2"
+        print("pression touch 2")
     }
     
     @IBAction func action_touch_3(sender: AnyObject) {
         calcul.text = "3"
+        print("pression touch 3")
     }
     @IBAction func action_touch_4(sender: AnyObject) {
         calcul.text = "4"
+        print("pression touch 4")
     }
     @IBAction func action_touch_5(sender: AnyObject) {
         calcul.text = "5"
+        print("pression touch 5")
     }
     @IBAction func action_touch_6(sender: AnyObject) {
         calcul.text = "6"
+        print("pression touch 6")
     }
     @IBAction func action_touch_7(sender: AnyObject) {
         calcul.text = "7"
+        print("pression touch 7")
     }
     @IBAction func action_touch_8(sender: AnyObject) {
         calcul.text = "8"
+        print("pression touch 8")
     }
     @IBAction func action_touch_9(sender: AnyObject) {
         calcul.text = "9"
+        print("pression touch 9")
     }
+    @IBAction func action_touch_ac(sender: AnyObject) {
+        print("pression touch ac")
+    }
+    
+    @IBAction func action_touch_neg(sender: AnyObject) {
+        print("pression touch neg")
+    }
+    
+    @IBAction func action_touch_fois(sender: AnyObject) {
+        print("pression touch multiplication")
+    }
+    
+    @IBAction func action_touch_division(sender: AnyObject) {
+        print("pression touch division")
+    }
+    
+    @IBAction func action_touch_moins(sender: AnyObject) {
+        print("pression touch moins")
+    }
+    
+    @IBAction func action_touch_plus(sender: AnyObject) {
+        print("pression touch plus")
+    }
+    
+    @IBAction func action_touch_egal(sender: AnyObject) {
+        print("pression touch egal")
+    }
+    
 }
 
