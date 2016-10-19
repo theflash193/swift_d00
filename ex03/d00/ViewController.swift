44e021c784feb8af04a1ff7f030dfa276977d400
Gordon RASS-KWASI <grass-kw@e2r5p5.42.fr>
2016-10-19 15:14:08 +0200

    le calcul est lie au code

--- ex03/d00/ViewController.swift
+++ ex03/d00/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var calcul: UILabel!
+    
     @IBOutlet weak var but_ac: UIButton!
     @IBOutlet weak var but_neg: UIButton!
     
