4b3ba0da0bb6f39dfadb300b5af9570db6c086bf
Gordon RASS-KWASI <grass-kw@e2r5p5.42.fr>
2016-10-19 15:04:57 +0200

    button lier au code

--- ex03/d00/ViewController.swift
+++ ex03/d00/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var but_ac: UIButton!
+    @IBOutlet weak var but_neg: UIButton!
+    
+    
+    @IBOutlet weak var but_9: UIButton!
+    @IBOutlet weak var but_8: UIButton!
+    @IBOutlet weak var but_7: UIButton!
+    @IBOutlet weak var but_6: UIButton!
+    @IBOutlet weak var but_5: UIButton!
+    @IBOutlet weak var but_4: UIButton!
+    @IBOutlet weak var but_3: UIButton!
+    @IBOutlet weak var but_2: UIButton!
+    @IBOutlet weak var but_1: UIButton!
+    @IBOutlet weak var but_0: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
